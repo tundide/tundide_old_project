67ecdd226c8eba5440c988940aff75ce6364e174
Marcos Panichella <mpanichella@live.com>
2017-07-06 23:28:53 -0300

    Se corrige el manejo del deploy

--- deploy.cmd
+++ deploy.cmd
@@ -87,10 +87,12 @@ goto :EOF
 
 :Deployment
 echo Handling node.js deployment.
+call npm install
+call npm run build
 
 :: 1. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;.vscode;docs;deploy.cmd"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
